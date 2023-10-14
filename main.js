f8b9f1e527586dbb42dd14085afa8f7fefa4c540
1996chithra <chithranair1996@gmail.com>
2023-10-14 23:45:14 +0530

    no changes

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-let popup = document.getElementById("popup");
 
-function openPopup(){
-            popup.classList.add("open-popup");
+          function changeclr(){
+
+            document.getElementById("color").style.background="red"
+            alert("Page is loading...")
+         
+
           }
-
-          function closePopup(){
-            popup.classList.remove("open-popup");
-          }
-
